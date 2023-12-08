f65717052baa44fab350388589e14ab083672344
ippei <ippei_kamimura@icloud.com>
2023-12-09 07:31:38 +0900

    time.AfterFunc

---
+++ timeafterfunc/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"time"
+)
+
+// 3秒ごとに繰り返し処理みたいなことができる
+func main() {
+	var timer *time.Timer
+	timer = time.AfterFunc(3*time.Second, func() {
+		println("Hello, world!")
+		timer.Reset(3 * time.Second)
+	})
+
+	sigCh := make(chan os.Signal, 1)
+
+	signal.Notify(sigCh, os.Interrupt)
+
+	log.Println(<-sigCh)
+}
